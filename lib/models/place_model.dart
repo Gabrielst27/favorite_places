4933cde8e034133e7db529cf09e06bb5203822b3
Gabriel Torres <gabrielst328@gmail.com>
2025-07-03 19:57:12 -0300

    feat: created place model and places list widget

---
+++ lib/models/place_model.dart
@@ -0,0 +1,11 @@
+class PlaceModel {
+  const PlaceModel({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  final String id;
+  final String name;
+  final String imageUrl;
+}
--- lib/screens/places_screen.dart
+++ lib/screens/places_screen.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 
 class PlacesScreen extends StatelessWidget {
@@ -20,7 +21,7 @@ class PlacesScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Placeholder(),
+      body: PlacesList(),
     );
   }
 }
